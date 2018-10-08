8451b508d5dbaf42efb03eba5d9f792225d1a8a1
mihneavlad <mihnel@yahoo.com>
2018-10-08 16:15:37 +0200

    Created post method.

--- routes/api.js
+++ routes/api.js
@@ -1,1 +1,28 @@
+const path = require('path');
+const express = require('express');
+const withDB = require('../lib/with-db');
+const questionsJSON = path.join(__dirname, '..', 'data', 'questions.json');
 
+const router = express.Router();
+
+router.use(express.urlencoded());
+router.use(express.json());
+
+const getQuestions = async function(db, req,res,next){
+		const data = await db.get('questions').value();
+    res.json(data)
+}
+
+const updateQuestion = async function(db, req, res, next){
+  console.log(req.body)
+  const data = await db.get("questions").push(req.body)
+
+  res.json(data)
+}
+
+
+
+router.get('/question', withDB(questionsJSON, getQuestions));
+router.post('/question', withDB(questionsJSON, updateQuestion));
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 const path = require('path');
 
 const withDB = require('./lib/with-db');
-
+const routerAPI = require("./routes/api")
 const express = require('express');
 const port = 3000;
 
-const studentsJSON = path.join(__dirname,  'data', 'questions.json');
 
 const app = express();
 
-app.get('/', 	withDB(studentsJSON, async function(db, req,res,next){
-
-		const data = await db.get('questions').value();
-    res.json(data)
-	}));
+app.use("/api",routerAPI)
 
 
 
